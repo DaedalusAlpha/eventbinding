e1a5a3496dc4f364bf00a3a391493f29e25a9462
DaedalusAlpha <mattrmcc@gmail.com>
2022-09-30 08:41:11 -0500

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'eventbinding';
+  title: string = 'eventbinding';
+  names: string[] = ['Fred', 'Julie', 'Sam', 'Olivia', 'Adam', 'Jennifer'];
+  changeTitle(newTitle: string): void {
+    this.title = newTitle;
+  }
+  newName = '';
+  addName() {
+    this.names.push(this.newName);
+  }
 }
